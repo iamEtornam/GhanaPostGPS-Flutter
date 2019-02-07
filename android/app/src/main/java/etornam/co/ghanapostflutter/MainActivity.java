a01a59adaf9f898095ff89b115cb5b060904ca51
regnex <sunumacbright@gmail.com>
2019-02-07 13:08:44 +0000

    Initial commit

---
+++ android/app/src/main/java/etornam/co/ghanapostflutter/MainActivity.java
@@ -0,0 +1,13 @@
+package etornam.co.ghanapostflutter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
