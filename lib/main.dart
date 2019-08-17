669f4fc50e68877a7cf598ef23e9d9c153e99c08
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-08-17 20:02:22 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:ghana_post_flutter/address_details_page.dart';
 import 'package:ghana_post_flutter/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+    final _mainColor = Color(0xff0677BD);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,9 +17,17 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.light,
         primaryColor: Colors.white,
         scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Roboto'
+        fontFamily: 'Roboto',
+        appBarTheme: AppBarTheme(
+          textTheme: TextTheme(title: TextStyle(color: _mainColor, fontWeight: FontWeight.bold,fontSize: 18)),
+          iconTheme: IconThemeData(color: _mainColor),
+          elevation: Platform.isIOS ? 0 : 4
+        )
       ),
       home: HomePage(),
+      routes: <String, WidgetBuilder>{
+          '/addressDetilsPage': (BuildContext context) => AddressDetailsPage(),
+      },
     );
   }
 }
