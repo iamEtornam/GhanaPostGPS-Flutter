ff51629504741253621b0e0058848b386bc56db5
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-10-20 14:11:48 +0000

    Create scan_address_page.dart

---
+++ lib/scan_address_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ScanAddressPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Center(child: Text('Scan qrcode or ghanapost gps code '))
+        ],
+      ),
+    );
+  }
+}
